fd974ad7821f77d5956fdfb58c420f4d651632a9
Peng Liu <myme5261314@gmail.com>
2016-04-18 01:09:43 +0800

    Add Dash layer.

--- init.el
+++ init.el
@@ -113,6 +113,7 @@ values."
      emacs-lisp
      markdown
      org
+     dash
      my)
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
